3812f1526cf6bf0c7511d7fdf495f4f9ffa31bf4
Jamie Jenks <jenksjm@gmail.com>
2022-12-05 14:50:08 +0100

    init commit

---
+++ src/main/java/com/manyotherroads/upchecker/UpcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.manyotherroads.upchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manyotherroads/upchecker/UpcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manyotherroads.upchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
